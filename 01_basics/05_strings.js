62aa57687c52c796e1262db8b0f0551c60a630f7
Gaurav Singh <35394758+gauravsingh890@users.noreply.github.com>
2023-12-29 02:52:12 +0000

    slice and substring mehod

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -1,4 +1,24 @@
 const name = "John Doe"
 const repoCount = 50
 
-console.log(name + " " + repoCount + " Value");
+console.log(name + " " + repoCount + " Value");     // Instead of using this use string interpolation
+
+// String interpolation -> can be changed on the go
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}.`);
+
+const gameName = new String('John-Doe')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));     // -1 as 't' is not present in string gameName
+console.log(gameName.indexOf('D')); 
+
+const newString = gameName.substring(0, 4)
+console.log(newString);
+
+const anotherString = gameName.slice(-6,3)
+console.log(anotherString);
