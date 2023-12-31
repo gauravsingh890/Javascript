41ae38b5771de805d755940bd02eb324ef95c759
Gaurav Singh <35394758+gauravsingh890@users.noreply.github.com>
2023-12-31 03:46:27 +0000

    strings, nums and maths

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -8,7 +8,7 @@ console.log(`Hello my name is ${name} and my repo count is ${repoCount}.`);
 
 const gameName = new String('John-Doe')
 
-console.log(gameName[0]);
+console.log(`gameName at 0 ${gameName[0]}`);
 console.log(gameName.__proto__);
 
 console.log(gameName.length);
@@ -18,7 +18,17 @@ console.log(gameName.indexOf('t'));     // -1 as 't' is not present in string ga
 console.log(gameName.indexOf('D')); 
 
 const newString = gameName.substring(0, 4)
-console.log(newString);
+console.log("new String",newString);
 
-const anotherString = gameName.slice(-6,3)
-console.log(anotherString);
+const anotherString = gameName.slice(-6,4)
+console.log("anotherString",anotherString);
+
+const newStringOne = "    john Doe    "
+console.log(`newStringOne is ${newStringOne}`);
+console.log(`newStringOne.trim() is ${newStringOne.trim()}`);
+
+const url = "https://johnDoe.com/john%20%doe"
+console.log(`url.replace('%20%', '-') will replace %20 with '-' ${url.replace('%20%','-')}`);
+console.log(`url.includes('john') ${url.includes('john')}`);
+
+console.log(gameName.split('-'));
---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,34 @@
+const score = 400
+console.log(score);
+
+const balance = new Number(999);
+console.log("balance value is ",balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const otherNumber = 123.8966
+
+console.log(otherNumber.toPrecision(4));    // return string :-> round off the value   "123.9"
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString('en-IN'));      // 10,00,000 :-> Indian number system   default set to us.
+
+// +++++++++++++++++++++++++ Maths +++++++++++++++++++++++++
+
+console.log(Math);
+console.log(Math.abs(-5));      // 5
+console.log(Math.round(5.5));   // 6
+console.log(Math.ceil(5.6));    // 6
+console.log(Math.floor(5.3));   // 5
+console.log(Math.min(4,3,6,7,9));   // 3
+console.log(Math.max(4,3,6,7,9));   // 9
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)));
