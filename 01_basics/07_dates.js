b1dab426d89a311c02eb7696268d0eb3f9ac103e
Gaurav Singh <35394758+gauravsingh890@users.noreply.github.com>
2024-01-01 18:29:26 +0000

    tolocaleString method

--- 01_basics/07_dates.js
+++ 01_basics/07_dates.js
@@ -43,7 +43,10 @@ console.log(newDate.getDay());      // 1    -> Monday
 console.log(`${newDate.getDay()} and the time `) // to write in specific format
 
 console.log(newDate.toLocaleString('default',{
-    weekday : "long"
+    weekday : "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric"
     // timeZone : 'UTC',
     // hour12 : false
 }));
