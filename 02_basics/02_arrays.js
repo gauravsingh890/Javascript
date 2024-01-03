33b34e469bf81ca9bb4b4297200b785cdcadada2
Gaurav Singh <35394758+gauravsingh890@users.noreply.github.com>
2024-01-03 17:45:35 +0000

    arrays.js completed

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -19,3 +19,14 @@ const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
 
 const usable_another_array = another_array.flat(3);     // 3 is the depth for the flat value to unpack the array    :-> can use infinity too
 console.log(usable_another_array);
+
+
+console.log(Array.isArray("John"));
+console.log(Array.from("John"));
+console.log(Array.from({nam: "John"}));     // we have to assign if the array should be formed with keys or values
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3));
